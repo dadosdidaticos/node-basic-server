80af9a3f489b22113b6559db4fc9a0be5faae36c
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-09-25 00:14:50 -0300

    Refatoração usando node express

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const http = require('http')
 const fs = require('fs')
 const port = process.env.PORT || 8080
 
-const server = http.createServer((req, res) => {
+const express = require('express');
+const app = express();
+
+app.get('/',routeHandler)
+app.get('/about',routeHandler)
+app.get('/contactme',routeHandler)
+app.use(notFound)
+
+const routeHandler = function (req,res) {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html');
-    let path = 'https://node-basic-server.marcos-vinici70.repl.co/'
-    switch(req.url){
-        case '/':
-            path += 'index.html'
-            break
-        case '/about':
-            path += 'about.html'
-            break
-        case '/contactme':
-            path += 'contactme.html'
-            break
-        default:
-            path += '404.html'
-            break;
-    }
-
-
-    fs.readFile(path, (err,data) => {
+    res.setHeader('Content-Type', 'text/html')
+    fs.readFile(`https://node-basic-server.marcos-vinici70.repl.co${req.url}.html`, (err,data) => {
         if (err){
             console.log(err);
         }else{
             res.end(data);
         }
-    })  
-})
+    })
+} 
 
-server.listen(port, () => {
+const notFound = function(req,res) {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/html')
+    fs.readFile(`https://node-basic-server.marcos-vinici70.repl.co/404.html`, (err,data) => {
+        if (err){
+            console.log(err);
+        }else{
+            res.end(data);
+        }
+    })
+}
+
+app.listen(port, () => {
   console.log(`Server running at port ${port}`)
 })
 
