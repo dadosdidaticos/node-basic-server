01c49533add5a6e342001ead25884781eac5a8d0
Dados Didáticos <94938589+dadosdidaticos@users.noreply.github.com>
2022-09-25 04:17:42 +0000

    Correção de erros na refatoração

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
 const port = process.env.PORT || 8080
-
 const express = require('express');
 const app = express();
 
@@ -9,31 +7,23 @@ app.get('/about',routeHandler)
 app.get('/contactme',routeHandler)
 app.use(notFound)
 
-const routeHandler = function (req,res) {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    fs.readFile(`https://node-basic-server.marcos-vinici70.repl.co${req.url}.html`, (err,data) => {
-        if (err){
-            console.log(err);
-        }else{
-            res.end(data);
-        }
-    })
-} 
-
-const notFound = function(req,res) {
-    res.statusCode = 404
-    res.setHeader('Content-Type', 'text/html')
-    fs.readFile(`https://node-basic-server.marcos-vinici70.repl.co/404.html`, (err,data) => {
-        if (err){
-            console.log(err);
-        }else{
-            res.end(data);
-        }
-    })
-}
-
 app.listen(port, () => {
   console.log(`Server running at port ${port}`)
 })
+
+function routeHandler (req,res) {
+    res.statusCode = 200
+    let path
+    req.url!=='/' ? path = `${__dirname}/${req.url}.html`: path = `${__dirname}/index.html`
+    res.sendFile(path, readErrHandler)
+} 
+
+ function notFound (req,res) {
+    res.statusCode = 404
+    res.sendFile(`${__dirname}/404.html`, readErrHandler)
+} 
+
+function readErrHandler (err) {
+  if (err){console.log(err)}
+}
 
